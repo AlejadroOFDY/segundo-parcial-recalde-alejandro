f9efd914634b3dd31a0dd5b515f3ba531062fa48
AlejandroOFDY <BB_Rae@hotmail.com>
2025-06-27 11:20:59 -0300

    Cambiando el contenido de controllers a donde corresponde (models) y ahora sí terminando database.js (en el commit anterior me equivoqué y coloqué database cuando hablaba de models)

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
 import express from "express";
 import bookRoutes from "./src/routes/book.routes.js";
 import dotenv from "dotenv";
-
+import { startDb } from "./src/config/database.js";
 
 dotenv.config();
 
 app.use(express.json());
 app.use("/api/books", bookRoutes)
 
-
-
 const PORT = process.env.PORT || 3000;
 
+app.listen(PORT, async () => {
+  await startDb();
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
 
 
 
---
+++ src/config/database.js
@@ -0,0 +1,24 @@
+import { Sequelize } from "sequelize";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
+  }
+);
+
+export const startDb = async () => {
+  try {
+  await sequelize.authenticate();
+  console.log('La base de datos se conectó correctamente');
+  await sequelize.sync();
+} catch (error) {
+  console.error('Se produjo un error al intentar conectar la DB', error);
+}
+}
